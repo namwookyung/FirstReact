babf7e3122978b891f4917b4910ae77e58189e24
Wookyung <namwookyung.dev@gmail.com>
2022-09-04 23:59:04 +0900

    댓글 컴포넌트 만들기_22.09.04

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments=[
+    {
+        name:"남우경",
+        comment:"안녕하세요, 나무입니다.",
+    },
+    {
+        name:"박은빈",
+        comment:"토깽이라는 어감이 좋아요.",
+    },
+    {
+        name:"아무개",
+        comment:"저는 아무개입니다.",
+    },
+];
+
 function CommentList(props){
     return (
         <div>
-            <Comment />
+            {comments.map((comment)=>{
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }
