45b63bb6a3c807bd5bceedc621be92d074df246e
Wookyung <namwookyung.dev@gmail.com>
2022-09-07 01:43:52 +0900

    Card 컴포넌트 만들기_22.09.07

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props){
+    return (
+        <Card title="Wookyung Nam" backgroundColor="#4ea04e">
+            <p>안녕하세요, 나무입니다.</p>
+            <p>저는 현재 리액트를 공부하고 있습니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
